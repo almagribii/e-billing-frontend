01fa4bd42b30bcaa75e9d5a5359c371fda384d95
Almagribii <brucadalm@gmail.com>
2025-11-01 20:37:56 +0700

    update root and main

--- src/Root.tsx
+++ src/Root.tsx
@@ -1,15 +1,18 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Layout from "./components/Layout";
 
 function App() {
-  return <div>
-    <Router>
-      <Routes>
-        <Route>
-          
-        </Route>
-      </Routes>
-    </Router>
-  </div>;
+  return (
+    <div>
+      <Router>
+        <Layout>
+          <Routes>
+            <Route></Route>
+          </Routes>
+        </Layout>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
