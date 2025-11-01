79b03b4149a653489253841183da7f8f07c4094d
Almagribii <brucadalm@gmail.com>
2025-11-02 06:10:28 +0700

    update root so that login page is appear first

--- src/Root.tsx
+++ src/Root.tsx
@@ -1,15 +1,17 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Layout from "./components/Layout";
+import Layout from "./layout/Layout";
+import Login from "./pages/public/login";
 
 function App() {
   return (
     <div>
       <Router>
-        <Layout>
-          <Routes>
-            <Route></Route>
-          </Routes>
-        </Layout>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/" element={<Layout />}>
+            <Route path="users" element={<div>Halaman User</div>} />
+          </Route>
+        </Routes>
       </Router>
     </div>
   );
